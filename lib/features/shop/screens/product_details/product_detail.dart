708d99f4e0f4f92ad915ac010cd958b274f33df4
Diwe innocent <73207442+mhista@users.noreply.github.com>
2024-02-24 17:17:35 +0100

    working on product details page

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -3,6 +3,7 @@ import 'package:iconsax/iconsax.dart';
 import 'package:pickafrika/common/widgets/appbar/appBar.dart';
 import 'package:pickafrika/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:pickafrika/common/widgets/icons/circular_icon.dart';
+import 'package:pickafrika/common/widgets/images/edge_rounded_images.dart';
 import 'package:pickafrika/utils/constants/colors.dart';
 import 'package:pickafrika/utils/constants/image_strings.dart';
 import 'package:pickafrika/utils/constants/sizes.dart';
@@ -16,44 +17,13 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isDark = PHelperFunctions.isDarkMode(context);
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: PCurvedEdgesWidget(
           child: Column(
             children: [
               // PRODUCT IMAGE SLIDER
-              Container(
-                color: isDark ? PColors.darkerGrey : PColors.light,
-                child: const Stack(
-                  children: [
-                    // MAIN LARGE IMAGE
-                    SizedBox(
-                      height: 370,
-                      child: Padding(
-                        padding: EdgeInsets.all(PSizes.productImageRadius * 2),
-                        child: Center(
-                          child: Image(
-                            image: AssetImage(PImages.productWomen8),
-                          ),
-                        ),
-                      ),
-                    ),
-
-                    //  IMAGE SLIDER
-                    PProductImageSlider(),
-                    // APPBAR ICONS
-                    PAppBar(
-                      showBackArrow: true,
-                      actions: [
-                        PCircularIcon(
-                          icon: Iconsax.heart5,
-                          color: PColors.error,
-                        )
-                      ],
-                    )
-                  ],
-                ),
-              ),
+              ProductImageSlider(),
             ],
           ),
         ),
--- lib/features/shop/screens/product_details/widgets/product_image_slider.dart
+++ lib/features/shop/screens/product_details/widgets/product_image_slider.dart
@@ -1,37 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
+import '../../../../../common/widgets/appbar/appBar.dart';
+import '../../../../../common/widgets/icons/circular_icon.dart';
 import '../../../../../common/widgets/images/edge_rounded_images.dart';
 import '../../../../../utils/constants/colors.dart';
 import '../../../../../utils/constants/image_strings.dart';
 import '../../../../../utils/constants/sizes.dart';
 import '../../../../../utils/helpers/helper_functions.dart';
 
-class PProductImageSlider extends StatelessWidget {
-  const PProductImageSlider({
+class ProductImageSlider extends StatelessWidget {
+  const ProductImageSlider({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
     final isDark = PHelperFunctions.isDarkMode(context);
-    return Positioned(
-      right: 0,
-      left: PSizes.spaceBtwSections,
-      bottom: 30,
-      child: SizedBox(
-        height: 75,
-        child: ListView.separated(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            physics: const AlwaysScrollableScrollPhysics(),
-            itemBuilder: (_, index) => PRoundedImage(
-                padding: const EdgeInsets.all(PSizes.sm),
-                border: Border.all(color: PColors.primary),
-                width: 75,
-                backgroundColor: isDark ? PColors.dark : PColors.white,
-                imageUrl: PImages.productWomen8),
-            separatorBuilder: (_, __) => const SizedBox(),
-            itemCount: 8),
+    return Container(
+      color: isDark ? PColors.darkerGrey : PColors.light,
+      child: Stack(
+        children: [
+          // MAIN LARGE IMAGE
+          const SizedBox(
+            height: 370,
+            child: Padding(
+              padding: EdgeInsets.all(PSizes.productImageRadius * 2),
+              child: Center(
+                child: Image(
+                  image: AssetImage(PImages.productWomen8),
+                ),
+              ),
+            ),
+          ),
+
+          //  IMAGE SLIDER
+          Positioned(
+            right: 0,
+            left: PSizes.spaceBtwSections,
+            bottom: 30,
+            child: SizedBox(
+              height: 75,
+              child: ListView.separated(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  itemBuilder: (_, index) => PRoundedImage(
+                      padding: const EdgeInsets.all(PSizes.sm),
+                      border: Border.all(color: PColors.primary),
+                      width: 75,
+                      backgroundColor: isDark ? PColors.dark : PColors.white,
+                      imageUrl: PImages.productWomen8),
+                  separatorBuilder: (_, __) => const SizedBox(),
+                  itemCount: 8),
+            ),
+          ),
+          // APPBAR ICONS
+          const PAppBar(
+            showBackArrow: true,
+            actions: [
+              PCircularIcon(
+                icon: Iconsax.heart5,
+                color: PColors.error,
+              )
+            ],
+          )
+        ],
       ),
     );
   }
